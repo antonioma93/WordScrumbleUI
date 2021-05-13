41cd979090f3243b3fb970cb8e727629baa53331
Antonio Massa <antonio.massa@zehus.it>
2021-05-13 16:13:38 +0200

    Initial Commit

---
+++ WordScrumble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScrumble
+//
+//  Created by Massa Antonio on 13/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScrumble/WordScrumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrumbleApp.swift
+//  WordScrumble
+//
+//  Created by Massa Antonio on 13/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
